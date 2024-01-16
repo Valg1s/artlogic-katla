9688d562828f05b9eb39a285552ee7ccc97fa155
Valg1s <antonsegodin@gmail.com>
2024-01-16 15:49:05 +0100

    Add restore button and restore func to HiveListComponent. Add setHiveStatus function to HiveService. Add buttons delete and undelete with similar name func`s to HiveSectionListComponent. Add func setHiveSectionStatus to HiveSectionService

--- KatlaSportNg/src/app/hive-management/services/hive.service.ts
+++ KatlaSportNg/src/app/hive-management/services/hive.service.ts
@@ -39,8 +39,11 @@ export class HiveService {
   }
 
   setHiveStatus(hiveId: number, deletedStatus: boolean): Observable<Object> {
-    return null;
+    return this.http.put(`${this.url}${hiveId}/status/${deletedStatus}`, {});
   }
 
+  setHiveSectionStatus(hiveSectionId: number, deletedStatus: boolean): Observable<Object> {
+    return this.http.put(`${environment.apiUrl}/api/sections/${hiveSectionId}/status/${deletedStatus}`, {})
+  }
   
 }
